9a81aef667607da69a80f875d3053ea045e93b4d
Hargaaya <hargaaya10@gmail.com>
2022-02-10 10:59:52 +0100

    Created weather.js

---
+++ Weather.js
@@ -0,0 +1,9 @@
+export default class Weather {
+  constructor() {
+    this.key = "80a21c47a4285bedd4a78e3deec371e2";
+    this.url = "api.openweathermap.org/data/2.5/onecall";
+
+    // Exempel
+    // api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely&appid=5aea0ff31a0b778fdf225480e5f7002f
+  }
+}
