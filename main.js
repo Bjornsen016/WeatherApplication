811384c970795d9264cb01f0f84e89858f910431
Hargaaya <hargaaya10@gmail.com>
2022-02-16 20:13:25 +0100

    Completed saved location functionality

--- main.js
+++ main.js
@@ -5,63 +5,103 @@ const backgrounds = [
   {
     name: "Rain",
     url: "https://media4.giphy.com/media/xUPGcdhiQf2vbfDCyk/giphy.gif?cid=ecf05e47kf91tsmns5sp0y6ch6pv0twmxmq86ukz38atf009&rid=giphy.gif&ct=g",
+    statement: "Just nu regnar det",
   },
   {
     name: "Snow",
     url: "https://media1.giphy.com/media/d3mlmtNPoxNrt4Bi/giphy.gif?cid=ecf05e470wuj51br6dtxnbzobljgfws7nibssishs4v0ghka&rid=giphy.gif&ct=g",
+    statement: "Just nu snöar det",
   },
   {
     name: "Clouds",
     url: "https://media0.giphy.com/media/d5PPYjcb3caPTHM3hv/giphy.gif?cid=ecf05e47nrydepn9iphbq9ubjouhmz7s9qlav40iqzabmnss&rid=giphy.gif&ct=g",
+    statement: "Just nu är det molnigt",
   },
   {
     name: "Clear",
     url: "https://media0.giphy.com/media/EKpmZuydbsmRy/giphy.gif?cid=ecf05e47rx85450iutghyhy7nd1y7td8k47al5ti8fdmd026&rid=giphy.gif&ct=g",
+    statement: "Just nu är det klart ute",
   },
   {
     name: "Drizzle",
     url: "https://cdn.dribbble.com/users/1951927/screenshots/5777363/drizzle-clouds-dribbble-dom-civiello1.gif",
+    statement: "Just nu regnar det lite",
   },
   {
     name: "Thunderstorm",
     url: "https://media2.giphy.com/media/IcTPgN5cI0Ij464QCa/giphy.gif?cid=ecf05e47sm7tstf4ekxz09ynbyi8zon77003yhtky8ssnbdz&rid=giphy.gif&ct=g",
+    statement: "Just nu råder oväder",
   },
 ];
 
-document.getElementById("nav").addEventListener("click", (e) => {
-  toggle(e.target.getAttribute("id"));
+document.getElementById("saved").addEventListener("click", (e) => {
+  let target = e.target;
+
+  if (target.tagName == "EM") {
+    const dropdown = document.getElementById("dropdown");
+
+    if (dropdown.style.display === "none") {
+      dropdown.style.display = "flex";
+
+      const submit = document.getElementById("submit");
+
+      submit.addEventListener("click", (e) => {
+        console.log("im running");
+        const form = document.getElementById("city");
+        addCity(form.value);
+        form.parentElement.reset();
+      });
+    } else {
+      dropdown.style.display = "none";
+    }
+  }
+
+  if (target.tagName === "LI") {
+    const city = target.innerText;
+    console.log(city);
+    // Kalla på funktionen som plockar fram vädret för staden
+  }
 });
 
-function renderNow(background, type) {
-  let targetWindow = document.getElementById("window");
-  let targetText = document.getElementById("nowText");
-  targetWindow.style.backgroundImage = `url(${background})`;
+document.getElementById("nav").addEventListener("click", (e) => {
+  const choice = e.target.getAttribute("id");
+  const clear = () => {
+    document.getElementById("main").innerHTML = "";
+  };
 
-  switch (type) {
-    case "Rain":
-      targetText.innerText = "Just nu regnar det";
+  switch (Number(choice)) {
+    case 1:
+      clear();
+      weather.setBackground(renderNow, backgrounds);
       break;
-
-    case "Snow":
-      targetText.innerText = "Just nu snöar det";
+    case 2:
+      clear();
+      weather.getHourly(renderToday);
       break;
-
-    case "Clouds":
-      targetText.innerText = "Just nu är det molnigt";
-      break;
-
-    case "Clear":
-      targetText.innerText = "Just nu är det klart ute";
-      break;
-
-    case "Drizzle":
-      targetText.innerText = "Just nu regnar det lite";
-      break;
-
-    case "Thunderstorm":
-      targetText.innerText = "Just nu råder oväder";
+    case 3:
       break;
   }
+});
+
+function addCity(city) {
+  const target = document.getElementById("savedList");
+  target.innerHTML += `<li>${city}</li>`;
+}
+
+function renderNow(background, type) {
+  const main = document.getElementById("main");
+  const statement = backgrounds.filter((item) => item.name === type)[0]
+    ?.statement;
+
+  main.innerHTML = `
+  <div id="now">
+    <div id="window"></div>
+    <h1 id="nowText">${statement}</h1>
+  </div>`;
+
+  document.getElementById(
+    "window"
+  ).style.backgroundImage = `url('${background}')`;
 }
 
 function renderToday(item) {
@@ -71,7 +111,7 @@ function renderToday(item) {
     minute: "2-digit",
   });
 
-  todayDiv.innerHTML += `
+  main.innerHTML += `
     <article>
         <h1>${time}</h1>
         <h2>${Math.floor(item.temp - 273.15) + " Grader"}</h2>
@@ -84,4 +124,4 @@ function renderToday(item) {
 }
 
 weather.setBackground(renderNow, backgrounds);
-weather.getHourly(renderToday);
+// weather.getHourly(renderToday);
